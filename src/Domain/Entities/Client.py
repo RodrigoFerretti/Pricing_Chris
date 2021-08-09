668dceeb9f5c4c6a66fc7751158798a9043a098f
Rodrigo <rodrigo_ferretti@hotmail.com>
2021-08-09 19:34:58 -0300

    getting minimum price for negotiation

--- src/Domain/Entities/Client.py
+++ src/Domain/Entities/Client.py
@@ -9,3 +9,26 @@ class Client:
         self.tpv = tpv
         self.segment_id = segment_id
         self.location_id = location_id
+
+    
+    def get_client_level(self: object, highest_tpv: Decimal):
+        tpv_levels = {
+            "1": 
+            {
+                "threshold": highest_tpv / 3
+            },
+            "2": 
+            {
+                "threshold": highest_tpv * 2 / 3
+            },
+            "3": 
+            {
+                "threshold": highest_tpv
+            }
+        }
+        client_level = 1
+        for level, level_properties in tpv_levels.items():
+            if self.tpv <= level_properties['threshold']:
+                client_level = int(level)
+                return client_level
+        return client_level
--- src/Service/Negotiation.py
+++ src/Service/Negotiation.py
@@ -11,13 +11,22 @@ from Domain.Entities.State import State
 from Domain.Entities.StatePrice import StatePrice
 from Domain.Entities.Segment import Segment
 
-from Domain.DBModels.Product import ProductModel
 from Domain.DBModels.LocationPrice import LocationPriceModel, LocationModel
 from Domain.DBModels.CityPrice import CityPriceModel, CityModel
 from Domain.DBModels.StatePrice import StatePriceModel, StateModel
 from Domain.DBModels.Segment import SegmentModel
+from Domain.DBModels.Client import ClientModel
 
 from Domain.DTO.Negotiation import NegotiationDTO
+
+
+def get_highest_client_tpv():
+    highest_tpv = ClientModel(
+        **ClientModel.query.order_by(
+            ClientModel.tpv.desc()
+            ).first().as_dict()
+        ).tpv
+    return highest_tpv
 
 
 class Negotiation:
@@ -34,10 +43,12 @@ class Negotiation:
         self.state = self.get_state()
         self.state_price = self.get_state_price()
         self.segment = self.get_segment()
+        self.level = self.get_negotiation_level()
 
     def get_location(self):
         location = Location(
-            **LocationModel.query.filter(
+            **LocationModel.query
+            .filter(
                 LocationModel.id == self.client.location_id
             ).first().as_dict()
         )
@@ -45,17 +56,19 @@ class Negotiation:
 
     def get_location_price(self):
         location_price = LocationPrice(
-            **LocationPriceModel.query.filter(
-                ProductModel.id == self.product.id
+            **LocationPriceModel.query
+            .filter(
+                LocationPriceModel.location_id == self.location.id
             ).filter(
-                LocationModel.id == self.location.id
+                LocationPriceModel.product_id == self.product.id
             ).first().as_dict()
         )
         return location_price
 
     def get_city(self):
         city = City(
-            **CityModel.query.filter(
+            **CityModel.query
+            .filter(
                 CityModel.id == self.location.city_id
             ).first().as_dict()
         )
@@ -63,7 +76,8 @@ class Negotiation:
     
     def get_city_price(self):
         city_price = CityPrice(
-            **CityPriceModel.query.filter(
+            **CityPriceModel.query
+            .filter(
                 CityPriceModel.city_id == self.city.id
             ).filter(
                 CityPriceModel.product_id == self.product.id
@@ -97,8 +111,32 @@ class Negotiation:
         )
         return segment
 
+    def get_prices(self):
+        prices = [
+            self.location_price.price, 
+            self.city_price.price, 
+            self.state_price.price
+        ]
+        prices.sort()
+        prices.reverse()
+        return prices
+
+    def get_negotiation_level(self):
+        highest_level = max(
+            [
+                self.seller.type, 
+                self.client.get_client_level(
+                    get_highest_client_tpv()
+                )
+            ]
+        )
+        return highest_level
+
     def profits_and_looses(self):
-        return None
+        possible_prices = self.get_prices()
+        minimum_price = possible_prices[self.level - 1]
+        return minimum_price
+
     
     def price_offer_is_higher_than_minimum(self):
         return None
@@ -108,9 +146,15 @@ new_negotiation = Negotiation(
     **NegotiationDTO(
         **{
             "client_id": 1, 
-            "seller_id": 2, 
+            "seller_id": 3, 
             "product_id": 3, 
             "price_offer": 500.00
         }
     ).__dict__
 )
+
+print(new_negotiation.level)
+
+print(new_negotiation.get_prices())
+
+print(new_negotiation.profits_and_looses())
