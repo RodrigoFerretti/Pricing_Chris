7cedbd53e3e58e03d4684222a78f428756840fdb
Rodrigo <rodrigo_ferretti@hotmail.com>
2021-08-12 17:24:03 -0300

    adding pymysql due to docker errors

--- src/Setup/API.py
+++ src/Setup/API.py
@@ -2,13 +2,14 @@ import os
 from flask import Flask
 import dotenv
 from flask_swagger_ui import get_swaggerui_blueprint
+import pymysql
 
 dotenv.load_dotenv()
 
 
 app: Flask = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql://{os.environ['db_user']}:{os.environ['db_password']}" \
+app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+pymysql://{os.environ['db_user']}:{os.environ['db_password']}" \
                                         f"@{os.environ['db_host']}:{os.environ['db_port']}/{os.environ['db_schema']}"
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
